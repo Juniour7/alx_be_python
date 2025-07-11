c4a27297c20dd0b32e66c106cc5a3122528b8c0a
Junior7 <giftmomanyi97@gmail.com>
2025-07-11 08:01:32 +0300

    New file for financing monthly salary

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,18 @@
+# We are trying to calculate a user's monthly salary based on inputed monthly income and expenses.
+# Prompt the user to input their monthly salary
+# Prompt the user for thier monthly expenses
+# Calculate mothly savings by subtracting expenses from the monthly salary
+# Project annual savings assuming a simple annual interest rate of 5%
+# Project savings after one year, incoporating the interest
+# Display the user's monthly savings
+# Display the projected annual savings after including the interest
+
+monthly_salary = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total mothly expenses: "))
+
+monthly_savings = monthly_salary - monthly_expenses
+
+project_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print("Your monthly savings are ", monthly_savings)
+print("Projected savings after one year, with interest, is", project_annual_savings)
