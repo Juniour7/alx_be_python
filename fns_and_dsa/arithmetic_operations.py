e6466d4c6719d5ec383d045c56b7660cabfe8e8f
Junior7 <giftmomanyi97@gmail.com>
2025-07-27 23:21:24 +0300

    fice

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,3 +1,4 @@
+["num1", "num2", "operation"]
 def perform_operation(num1,num2,operation):
     if operation.lower() == 'add':
         result = num1 + num2
@@ -6,7 +7,9 @@ def perform_operation(num1,num2,operation):
     elif operation.lower() == 'multiply':
         result = num1 * num2
     elif operation.lower() == 'divide':
-        result = num1 /num2
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        result = num1 / num2
     else :
         print("Enter the operation (add, subtract, multiply, divide): ")
     return result
