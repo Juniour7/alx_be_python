70e4fcfc61993e23660dbdcc64237a88c426b043
Junior7 <giftmomanyi97@gmail.com>
2025-07-30 15:33:26 +0300

    Back Account - 1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    """This Class interfaces with main.py to use command line inputs in order to perfor banking operations"""
+    def __init__(self, account_balance = 0 ):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        return self.account_balance
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            return True
+        else :
+            return False
+        
+    def display_balance(self):
+        print(f"You balance is {self.account_balance}")
+    
+    
+        
